0f12835c39ab5b49951ff2cacbc3c1168b34d84a
BK <ck.comcave@proton.me>
2024-02-01 10:21:17 +0100

    Interfaces Bird

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Bird implements Flyable,IFeathers{
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return "I'm a bird, i can fly!";
+    }
+
+    @Override
+    public String hasFeathers() {
+        // TODO Auto-generated method stub
+        return "I'm a bird, I have feathers!";
+    }
+
+
+
+
+}
